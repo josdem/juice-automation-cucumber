f7c900eb2607b4dcae7b01c9095fe564fdcba0e8
josdem <joseluis.delacruz@gmail.com>
2022-06-01 08:38:47 -0400

    Starting new webclient API automation with cucumber project :)

---
+++ src/main/java/com/josdem/jugoterapia/webclient/automation/JuiceAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.josdem.jugoterapia.webclient.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuiceAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuiceAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josdem/jugoterapia/webclient/automation/JuiceAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdem.jugoterapia.webclient.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuiceAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
