c0a04002b63accc164d0c140135645536a36b512
josdem <joseluis.delacruz@gmail.com>
2022-06-20 15:00:20 -0400

    #1 Making it works

--- src/main/java/com/josdem/jugoterapia/webclient/automation/JuiceAutomationApplication.java
+++ src/main/java/com/josdem/jugoterapia/webclient/automation/JuiceAutomationApplication.java
@@ -2,10 +2,13 @@ package com.josdem.jugoterapia.webclient.automation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.josdem.jugoterapia.webclient")
+@PropertySource("classpath:application.properties")
 public class JuiceAutomationApplication {
 
   public static void main(String[] args) {
--- src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesJsonNodeTest.java
+++ src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesJsonNodeTest.java
@@ -1,6 +1,5 @@
 package com.josdem.jugoterapia.webclient.automation;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.josdem.jugoterapia.webclient.automation.config.TestDataSource;
 import com.josdem.jugoterapia.webclient.service.BeverageService;
 import com.josdem.jugoterapia.webclient.service.CategoryService;
@@ -10,50 +9,41 @@ import cucumber.api.java.en.When;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class BeverageByCategoriesJsonNodeTest extends JuiceIntegrationTest {
 
-  private static final int EXPECTED_CATEGORY_ID = 5;
-  private static final int EXPECTED_BEVERAGE_ID = 85;
+    private static final int EXPECTED_CATEGORY_ID = 5;
+    private static final int EXPECTED_BEVERAGE_ID = 85;
 
-  private final CategoryService categoryService;
-  private final BeverageService beverageService;
-  private final TestDataSource data;
+    private final CategoryService categoryService;
+    private final BeverageService beverageService;
+    private final TestDataSource data;
 
-  @Given("List of categories")
-  public void shouldGetCategories() {
-    log.info("Running: List of categories");
-    Mono<List<String>> publisher =
-            categoryService.getCategoriesByLanguageJson("en").map(it -> it.findValuesAsText("id"));
-    StepVerifier.create(publisher)
-            .assertNext(
-                    categoriesIds -> {
-                      assertTrue(
-                              categoriesIds.contains(String.valueOf(EXPECTED_CATEGORY_ID)),
-                              "it should contains healthy category");
-                    })
-            .verifyComplete();
-  }
+    @Given("List of categories")
+    public void shouldGetCategories() {
+        log.info("Running: List of categories");
+        List<String> response =
+                categoryService.getCategoriesByLanguageJson("en").map(it -> it.findValuesAsText("id")).block();
+        assertTrue(
+                response.contains(String.valueOf(EXPECTED_CATEGORY_ID)),
+                "it should contains healthy category");
+    }
 
-  @When("I get beverages by category")
-  public void shouldGetBeveragesById() {
-    log.info("Running: I get beverages by category");
-    assertTrue(true);
-  }
+    @When("I get beverages by category")
+    public void shouldGetBeveragesById() {
+        log.info("Running: I get beverages by category");
+        assertTrue(true);
+    }
 
-  @Then("I get specific beverage")
-  public void shouldGetSpecificBeverage() {
-    log.info("Running: I get specific beverage");
-    assertTrue(true);
-  }
+    @Then("I get specific beverage")
+    public void shouldGetSpecificBeverage() {
+        log.info("Running: I get specific beverage");
+        assertTrue(true);
+    }
 }
