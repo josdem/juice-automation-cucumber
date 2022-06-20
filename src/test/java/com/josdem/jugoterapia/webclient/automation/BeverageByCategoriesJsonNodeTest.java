1bf9ccd41d0e933c5af13e598bd386cd614474a8
josdem <joseluis.delacruz@gmail.com>
2022-06-20 13:39:23 -0400

    #1 Changing System out by log info

--- src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesJsonNodeTest.java
+++ src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesJsonNodeTest.java
@@ -3,26 +3,28 @@ package com.josdem.jugoterapia.webclient.automation;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@Slf4j
 public class BeverageByCategoriesJsonNodeTest extends JuiceIntegrationTest {
 
   @Given("List of categories")
   public void shouldGetCategories() {
-    System.out.println("Running: List of categories");
+    log.info("Running: List of categories");
     assertTrue(true);
   }
 
   @When("I get beverages by category")
   public void shouldGetBeveragesById() {
-    System.out.println("Running: I get beverages by category");
+    log.info("Running: I get beverages by category");
     assertTrue(true);
   }
 
   @Then("I get specific beverage")
   public void shouldGetSpecificBeverage() {
-    System.out.println("Running: I get specific beverage");
+    log.info("Running: I get specific beverage");
     assertTrue(true);
   }
 }
