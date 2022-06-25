9cf94297799c72712a0d45cd5948c5cef4f5f4be
josdem <joseluis.delacruz@gmail.com>
2022-06-25 12:43:47 -0400

    #2 Changing payloads to a map

--- src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesMapTest.java
+++ src/test/java/com/josdem/jugoterapia/webclient/automation/BeverageByCategoriesMapTest.java
@@ -1,23 +1,21 @@
 package com.josdem.jugoterapia.webclient.automation;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.josdem.jugoterapia.webclient.automation.config.TestDataSource;
 import com.josdem.jugoterapia.webclient.service.BeverageService;
 import com.josdem.jugoterapia.webclient.service.CategoryService;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
-
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Slf4j
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -33,14 +31,13 @@ public class BeverageByCategoriesMapTest extends JuiceIntegrationTest {
   @Given("List of map categories")
   public void shouldGetCategories() {
     log.info("Running: List of categories");
-    Mono<List<String>> publisher =
-        categoryService.getCategoriesByLanguageJson("en").map(it -> it.findValuesAsText("id"));
+    Flux<Map> publisher =
+        categoryService.getCategoriesByLanguageMap("en").filter(it -> (int) it.get("id") == 5);
     StepVerifier.create(publisher)
         .assertNext(
-            categoriesIds -> {
-              assertTrue(
-                  categoriesIds.contains(String.valueOf(EXPECTED_CATEGORY_ID)),
-                  "it should contains healthy category");
+            category -> {
+              assertEquals(5, ((Map) category).get("id"));
+              assertEquals("Healing", ((Map) category).get("name"));
             })
         .verifyComplete();
   }
@@ -48,16 +45,17 @@ public class BeverageByCategoriesMapTest extends JuiceIntegrationTest {
   @When("I get a map beverages by category")
   public void shouldGetBeveragesById() {
     log.info("Running: I get beverages by category");
-    Mono<List<String>> publisher =
+    Flux<Map> publisher =
         categoryService
-            .getBeveragesByCategoryJson(EXPECTED_CATEGORY_ID)
-            .map(it -> it.findValuesAsText("id"));
+            .getBeveragesByCategoryMap(EXPECTED_CATEGORY_ID)
+            .filter(it -> (int) it.get("id") == 85);
     StepVerifier.create(publisher)
         .assertNext(
-            beveragesIds -> {
-              assertTrue(
-                  beveragesIds.contains(String.valueOf(EXPECTED_BEVERAGE_ID)),
-                  "it should contains Anti-constipation Smoothie beverage");
+            beverage -> {
+              assertEquals(data.getBeverage().getId(), beverage.get("id"));
+              assertEquals(data.getBeverage().getName(), beverage.get("name"));
+              assertEquals(data.getBeverage().getIngredients(), beverage.get("ingredients"));
+              assertEquals(data.getBeverage().getImage(), beverage.get("image"));
             })
         .verifyComplete();
   }
@@ -65,22 +63,19 @@ public class BeverageByCategoriesMapTest extends JuiceIntegrationTest {
   @Then("I get a map specific beverage")
   public void shouldGetSpecificBeverage() {
     log.info("Running: I get specific beverage");
-    Mono<JsonNode> publisher = beverageService.getBeverageAsJson(EXPECTED_BEVERAGE_ID);
+    Mono<Map> publisher = beverageService.getBeverageAsMap(EXPECTED_BEVERAGE_ID);
     StepVerifier.create(publisher)
         .assertNext(
             beverage -> {
               assertAll(
                   "beverage",
-                  () -> assertEquals(data.getBeverage().getId(), beverage.get("id").asInt()),
-                  () -> assertEquals(data.getBeverage().getName(), beverage.get("name").asText()),
+                  () -> assertEquals(data.getBeverage().getId(), beverage.get("id")),
+                  () -> assertEquals(data.getBeverage().getName(), beverage.get("name")),
                   () ->
                       assertEquals(
-                          data.getBeverage().getIngredients(),
-                          beverage.get("ingredients").asText()),
-                  () ->
-                      assertEquals(data.getBeverage().getRecipe(), beverage.get("recipe").asText()),
-                  () ->
-                      assertEquals(data.getBeverage().getImage(), beverage.get("image").asText()));
+                          data.getBeverage().getIngredients(), beverage.get("ingredients")),
+                  () -> assertEquals(data.getBeverage().getRecipe(), beverage.get("recipe")),
+                  () -> assertEquals(data.getBeverage().getImage(), beverage.get("image")));
             })
         .verifyComplete();
   }
